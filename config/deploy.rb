3ea775a477c6155ce0bea5d09d8b8c409ecfa548
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-05-14 17:33:47 +0800

    Remove delay_job in Cap file

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,7 +50,6 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       invoke 'puma:stop'
       invoke 'puma:start'
-      execute " RAILS_ENV=production #{release_path}/bin/delayed_job -n 2 restart"
     end
   end
 
