e898582a1864d51c7552bd6323325b2aa6582335
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-05-16 09:13:06 +0800

    Try using thread in ci_jobs

--- app/models/ci_job.rb
+++ app/models/ci_job.rb
@@ -1,10 +1,13 @@
 class CiJob < ApplicationRecord
   belongs_to :target
   def start_job
-    log = "#{ENV['HOME']}/ci_logs/#{self.target_name}_latest.log"
-    cmd = "cd ${HOME}/trauc-manual/contrib && ./scripts/run_z3_branch_by_cron.sh #{self.target_name} #{self.target_id} &> #{log}"
-    #%x{ #{cmd} }
-    system(cmd)
-    self.update_attribute(:status, 0)
+    Thread.new do
+      log = "#{ENV['HOME']}/ci_logs/#{self.target_name}_latest.log"
+      cmd = "cd ${HOME}/trauc-manual/contrib && ./scripts/run_z3_branch_by_cron.sh #{self.target_name} #{self.target_id} &> #{log}"
+      puts cmd
+      #%x{ #{cmd} }
+      system(cmd)
+      self.update_attribute(:status, 0)
+    end
   end
 end
