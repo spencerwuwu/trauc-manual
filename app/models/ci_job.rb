73c81e9eee2c820886c8458bd6735e3eb4a763b8
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-05-16 10:55:03 +0800

    Try fixing job waits

--- app/models/ci_job.rb
+++ app/models/ci_job.rb
@@ -1,13 +1,15 @@
 class CiJob < ApplicationRecord
   belongs_to :target
   def start_job
-    Thread.new do
+    nt = Thread.new do
       log = "#{ENV['HOME']}/ci_logs/#{self.target_name}_latest.log"
-      cmd = "cd ${HOME}/trauc-manual/contrib && ./scripts/run_z3_branch_by_cron.sh #{self.target_name} #{self.target_id} &> #{log}"
-      puts cmd
+      cmd = "cd ${HOME}/trauc-manual/contrib && ./scripts/run_z3_branch_by_cron.sh #{self.target_name} #{self.target_id} > #{log} 2>&1"
+      logger.debug cmd
       #%x{ #{cmd} }
       system(cmd)
-      self.update_attribute(:status, 0)
+      logger.debug "Finish #{cmd}"
     end
+    nt.join
+    self.update_attribute(:status, 0)
   end
 end
