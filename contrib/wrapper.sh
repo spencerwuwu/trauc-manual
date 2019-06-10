1924db42d2133d3ee64696a7216ffd2de5b0deb2
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-06-10 13:26:59 +0800

    Fix script passwd error

--- contrib/wrapper.sh
+++ contrib/wrapper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ./scripts/run_z3_branch_by_cron.sh $1 $2
+export PGPASSWORD=$CI_DB_PASSWORD
 psql \
     -U pguser \
     -d ci \
-    -c "UPDATE ci_jobs SET status = 0 WHERE target_id = $2"
+    -c "UPDATE ci_jobs SET status = 0 WHERE target_id = $2" 
